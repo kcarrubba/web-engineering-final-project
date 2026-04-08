15cf33eb18bf2f986edec77dd695052a952a7cbd
haeun-yang <c3413958@email.com>
2026-04-08 16:22:15 +1000

    Add EnrolController and SemestersController with basic structure

---
+++ src/main/java/com/UniX/controllers/SemestersController.java
@@ -0,0 +1,36 @@
+package com.UniX.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.UniX.repositories.SemesterRepository;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/semsesters")
+public class SemestersController {
+
+    
+    private final SemesterRepository semesterRepository;
+
+
+}
