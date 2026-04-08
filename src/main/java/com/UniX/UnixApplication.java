b34e3aee2907d49fa9ebd73c2f6b3561ca5a35b0
Kaley Carrubba <c3571255@uon.edu.au>
2026-04-08 05:58:34 +0000

    Update demo files to correct naming

--- src/main/java/com/UniX/UnixApplication.java
+++ src/main/java/com/UniX/UnixApplication.java
@@ -1,5 +1,12 @@
 package com.UniX;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class UnixApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnixApplication.class, args);
+    }
 }
