df4775c7679bea7bd22150033e1364a0a38f6a32
Kaley Carrubba <c3571255@uon.edu.au>
2026-04-08 14:28:52 +0000

    Create API 2

--- src/main/java/com/UniX/controllers/EnrolController.java
+++ src/main/java/com/UniX/controllers/EnrolController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/enrol")
+@RequestMapping("/unix/enrol")
 public class EnrolController {
 
 
