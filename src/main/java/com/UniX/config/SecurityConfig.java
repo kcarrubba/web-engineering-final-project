af8792da23ca56c76051feea6fb0a14ae03433fb
haeun-yang <c3413958@email.com>
2026-04-15 20:47:02 +1000

    fix: set login api PermitAll

--- src/main/java/com/UniX/config/SecurityConfig.java
+++ src/main/java/com/UniX/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 "/style.css",
                                 "/images/**",
                                 "/login",
+                                "/unix/login",
                                 "/semesters",
                                 "/courses",
                                 "/course/**"
